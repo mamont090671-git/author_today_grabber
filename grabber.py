ca4a128fe27d81c6b35f7f9843e1e5b8841bb3af
mamont090671-git <63111671+mamont090671-git@users.noreply.github.com>
2024-04-28 15:24:23 +0500

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -1,1 +1,14 @@
 
+url = 'https://author.today/reader/12637/1056774'  #   TODO    ссылка на первую главу книги
+login = 'логин'    #   TODO    строка с логином
+password = 'чячячяч'   #   TODO    пароль
+file_name = 'out_text.txt'  #   TODO    файл куда будет копироваться книга
+fb2_file_out = 'книга.fb2'
+tmp_file = 'tmp.txt'
+web_hidden = False  #   TODO    выполнять со скрытым окном браузера
+disable_notification = True #   TODO    запрет уведомлений
+disable_image = True   #   TODO    работает через жопу, теряются главы
+start_maximized = False #   TODO    развернуть окно браузера
+mobileEmulation = False
+fb2_enable = True
+url_enable = True
